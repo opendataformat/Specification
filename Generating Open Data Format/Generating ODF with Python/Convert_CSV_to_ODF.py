3f9ac147710cb38873c0b2a15b8350d983931130
thartl-diw <160223397+thartl-diw@users.noreply.github.com>
2025-03-11 11:34:00 +0100

    Update Convert_CSV_to_ODF.py

--- Generating Open Data Format/Generating ODF with Python/Convert_CSV_to_ODF.py
+++ Generating Open Data Format/Generating ODF with Python/Convert_CSV_to_ODF.py
@@ -1,10 +1,12 @@
-# version 2.1.0 (03 December 2024)
+# version 2.2.0 (11 March 2025)
 
 # Modules
 import csv
 import os
 import shutil
+import zipfile
 import xml.etree.ElementTree as ET
+import json
 
 
 #########Inputs:###########
@@ -19,21 +21,22 @@ output_dir='C:/Users/thartl/Documents/soep datasets/datasetname'
 languages='all'
 #Datensatz (data.csv) mitexportieren?
 export_data='yes'
+#ODF version
+odf_version="1.1.0"
 
 
 ############# Functions for conversion to ODF #############
-
 # formatting xml output
 def pretty_print(current, parent=None, index=-1, depth=0):
   for i, node in enumerate(current):
     pretty_print(node, current, i, depth + 1)
   if parent is not None:
     if index == 0:
-      parent.text = '\n' + ('\t' * depth)
+      parent.text = '\n' + ('  ' * depth)
     else:
-      parent[index - 1].tail = '\n' + ('\t' * depth)
+      parent[index - 1].tail = '\n' + ('  ' * depth)
     if index == len(parent) - 1:
-      current.tail = '\n' + ('\t' * (depth - 1))
+      current.tail = '\n' + ('  ' * (depth - 1))
 
 # get head of csv file              
 def header(input_dir,csv_file):
@@ -65,7 +68,7 @@ def copy_data_csv(input_dir, output_dir, export_data):
       output_dir)
 
 # make xml file from csv files          
-def csv2xml(input_dir, output_dir):
+def csv2xml(input_dir, output_dir, odf_version):
   schema_name=ET.QName("http://www.w3.org/2001/XMLSchema-instance","schemaLocation")
   root = ET.Element("codeBook",
   {schema_name:"ddi:codebook:2_5 http://www.ddialliance.org/Specification/DDI-Codebook/2.5/XMLSchema/codebook.xsd",
@@ -73,69 +76,71 @@ def csv2xml(input_dir, output_dir):
   # check if location output directory exists/is valid
   dir_name = output_dir.replace('\\','/')
   dir_name = output_dir.split('/')
-  output_dir_name = dir_name[ len(dir_name)-1]
   root_dir=dir_name
   del(root_dir[len(root_dir)-1])
   root_dir = '/'.join(root_dir)
-  if os.access(root_dir, os.W_OK) == True:
-    # dataset
-    # check if dataset.csv exists
-    if os.path.exists(input_dir+"/dataset.csv") == True :
-      #study description
-      stdyDscr=ET.SubElement(root,'stdyDscr')
-      citation=ET.SubElement(stdyDscr,'citation')
-      titlStmt=ET.SubElement(citation,'titlStmt')
-      
-      #file description
-      fileDscr=ET.SubElement(root,'fileDscr')
-      fileTxt=ET.SubElement(fileDscr,'fileTxt')
-      with open(input_dir+"/dataset.csv",mode="r",encoding="utf-8") as file:
-        dict_reader=csv.DictReader(file)
-        for row in dict_reader:
-          
-          # dataset name
-          fileName=ET.SubElement(fileTxt,'fileName')
-          fileName.text=row['dataset']
-          
-          titl=ET.SubElement(titlStmt,'titl')
-          titl.text=row['study']
-          # get keys
-          list_keys = row.keys()
+  if os.access(root_dir, os.W_OK) == False:
+    raise ValueError('Invalid output directory: No writing permission for '+root_dir)
+  # dataset
+  # check if dataset.csv exists
+  if os.path.exists(input_dir+"/dataset.csv") == True :
+    #study description
+    stdyDscr=ET.SubElement(root,'stdyDscr')
+    citation=ET.SubElement(stdyDscr,'citation')
+    titlStmt=ET.SubElement(citation,'titlStmt')
+    
+    #file description
+    fileDscr=ET.SubElement(root,'fileDscr')
+    fileTxt=ET.SubElement(fileDscr,'fileTxt')
+    with open(input_dir+"/dataset.csv",mode="r",encoding="utf-8") as file:
+      dict_reader=csv.DictReader(file)
+      for row in dict_reader:
+        
+        # dataset name
+        fileName=ET.SubElement(fileTxt,'fileName')
+        fileName.text=row['dataset']
+        
+        titl=ET.SubElement(titlStmt,'titl')
+        titl.text=row['study']
+        # get keys
+        list_keys = row.keys()
 
-        # dataset label
-        fileCitation=ET.SubElement(fileTxt,'fileCitation')
-        titlStmt=ET.SubElement(fileCitation,'titlStmt')
-        first_label=0
-        if 'label' in list_keys:
-          titl=ET.SubElement(titlStmt,'titl')
-          titl.text=row['label']
-          first_label=1
-        if any(item.startswith('label_') for item in list_keys):
-          for lang in get_lang(input_dir,"dataset.csv","label"):
-            if first_label==0:
-              titl=ET.SubElement(titlStmt,'titl')
-              titl.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = lang
-              titl.text=row['label_'+lang]
-              first_label=1
-            else:
-              parTitl=ET.SubElement(titlStmt,'parTitl')
-              parTitl.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = lang
-              parTitl.text=row['label_'+lang]
-        # dataset description
-        if 'description' in list_keys:
+      # dataset label
+      fileCitation=ET.SubElement(fileTxt,'fileCitation')
+      titlStmt=ET.SubElement(fileCitation,'titlStmt')
+      first_label=0
+      if 'label' in list_keys:
+        titl=ET.SubElement(titlStmt,'titl')
+        titl.text=row['label']
+        first_label=1
+      if any(item.startswith('label_') for item in list_keys):
+        for lang in get_lang(input_dir,"dataset.csv","label"):
+          if first_label==0:
+            titl=ET.SubElement(titlStmt,'titl')
+            titl.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = lang
+            titl.text=row['label_'+lang]
+            first_label=1
+          else:
+            parTitl=ET.SubElement(titlStmt,'parTitl')
+            parTitl.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = lang
+            parTitl.text=row['label_'+lang]
+      # dataset description
+      if 'description' in list_keys:
+        fileCont=ET.SubElement(fileTxt,'fileCont')
+        fileCont.text=row['description']
+      if any(item.startswith('description_') for item in list_keys) == True:
+        for lang in get_lang(input_dir,"dataset.csv","description"):
           fileCont=ET.SubElement(fileTxt,'fileCont')
-          fileCont.text=row['description']
-        if any(item.startswith('description_') for item in list_keys) == True:
-          for lang in get_lang(input_dir,"dataset.csv","description"):
-            fileCont=ET.SubElement(fileTxt,'fileCont')
-            fileCont.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = lang
-            fileCont.text=row['description_'+lang]
-        # url
-        if 'url' in list_keys:
-          notes=ET.SubElement(fileDscr,'notes')
-          extlink=ET.SubElement(notes,'ExtLink')
-          extlink.attrib['URI']=row['url']
-
+          fileCont.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = lang
+          fileCont.text=row['description_'+lang]
+      # url
+      if 'url' in list_keys:
+        notes=ET.SubElement(fileDscr,'notes')
+        extlink=ET.SubElement(notes,'ExtLink')
+        extlink.attrib['URI']=row['url']
+  
+  if variables_arg == "yes" :
+    
     if os.path.exists(input_dir+"/categories.csv") == True :
         list_categories=[]
         categories=open(input_dir+"/categories.csv", mode="r", encoding="utf-8")
@@ -197,25 +202,41 @@ def csv2xml(input_dir, output_dir):
                   varFormat.attrib['type'] = row['type']                
               # Variable URL
               if 'url' in list_keys:    
-                if not row['url'] == '':
-                  notes = ET.SubElement(var, 'notes')
-                  ExtLink = ET.SubElement(notes, 'ExtLink')
-                  ExtLink.attrib['URI'] = row['url']
-                
-    # write xml
-    temp_output_dir=input_dir+'/'+output_dir_name
-    make_output_dir(temp_output_dir)
-    pretty_print(root)
-    tree = ET.ElementTree(root)
-    tree.write(temp_output_dir+"/metadata.xml", xml_declaration=True, encoding="utf-8")
-    copy_data_csv(input_dir, temp_output_dir, export_data)
-    shutil.make_archive(temp_output_dir, "zip", temp_output_dir)
-    shutil.copyfile(temp_output_dir+".zip",output_dir+'.zip')
-  else :
-    raise ValueError('Invalid output directory: No writing permission for '+root_dir)
+                notes = ET.SubElement(var, 'notes')
+                ExtLink = ET.SubElement(notes, 'ExtLink')
+                ExtLink.attrib['URI'] = row['url']
+              
+  # write xml
+  pretty_print(root)
+  tree = ET.ElementTree(root)
+  tree.write(input_dir+"/metadata.xml", xml_declaration=True, encoding="utf-8")
+  version_file = {
+    "fileType": "opendataformat",
+    "version": odf_version,
+    "files": {
+      "data": "data.csv",
+      "metadata": "metadata.xml"
+    }
+  }
+  # Write version file
+  with open(input_dir + '/odf-version.json', "w") as f:
+    json.dump(version_file, f, indent=2)  # Pretty formatting with indent
+  export_files = ["metadata.xml"]
+  if export_data == "yes":
+    export_files.append("data.csv")
+  if (odf_version.split('.')[0] == '1' and int(odf_version.split('.')[1]) >=1 or int(odf_version.split('.')[0]) > 2):
+    export_files.append("odf-version.json")
+  print(input_dir)
+  print(output_dir)
+  # Zip the output directory with all files
+  with zipfile.ZipFile(output_dir, "w") as zipf:
+    for file in export_files:
+      file_path = os.path.join(input_dir, file)
+      if os.path.isfile(file_path):  # Ensure it's a file, not a directory
+          zipf.write(file_path, arcname=file)  # Add to ZIP without full path
 
 
 
 
 ############## Convert CSVs to ODF #############
-csv2xml(input_dir, output_dir)
+csv2xml(input_dir, output_dir, odf_version)
